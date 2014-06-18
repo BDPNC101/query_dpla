f73c649a84f602874e7a68d8aba5de7c915e5667
timothyryanwalsh <timothyryanwalsh@gmail.com>
2014-06-17 20:08:27 -0600

    Update query_dpla.py

--- query_dpla.py
+++ query_dpla.py
@@ -53,7 +53,7 @@ class CustomStreamListener(tweepy.StreamListener):
         
         if total >= 1:    
         	api.update_status(".@%s %s %s" % (querier, title[:80], item_url), 
-            in_reply_to_status_id = status.id)
+        	in_reply_to_status_id = status.id)
         else:
         	api.update_status(".@%s No items found! Try another term!" % querier,
         	in_reply_to_status_id = status.id)           
