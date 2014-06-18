57c5576de360c4c11b8b4a20a0c274f73b675ef5
timothyryanwalsh <timothyryanwalsh@gmail.com>
2014-06-17 20:05:58 -0600

    Update query_dpla.py

--- query_dpla.py
+++ query_dpla.py
@@ -30,21 +30,34 @@ class CustomStreamListener(tweepy.StreamListener):
             
         # search DPLA
 
-        dpla = DPLA('xxxxx') # your DPLA API key here
-        result = dpla.search(q="%s" % twt_term, fields=["sourceResource.title", "id"])
+        dpla = DPLA('577312cc813353849a61ff27530eb6a0')
+        result = dpla.search(q="%s" % twt_term, fields=["sourceResource.title", "id"],
+        page_size=50)
             
-        # pick random result, fetch and prepare DPLA metadata
-    
-        json_data = result.items[random.randint(0,4)]
+        # pick random result, get and prepare DPLA metadata
+        
+        total = result.count
+        
+        if total > 49:
+        	json_data = result.items[random.randint(0, 49)]
+        elif total in range (1, 50):
+        	json_data = result.items[random.randint(0, (total - 1))]
+        else:
+        	pass
+        	
         title = json_data['sourceResource.title']
         json_id = json_data['id']
         item_url = 'http://dp.la/item/%s' % json_id
         
         # tweet DPLA metadata at querier
-            
-        api.update_status(".@%s %s %s" % (querier, title[:80], item_url), 
-        in_reply_to_status_id = status.id)
-            
+        
+        if total >= 1:    
+        	api.update_status(".@%s %s %s" % (querier, title[:80], item_url), 
+            in_reply_to_status_id = status.id)
+        else:
+        	api.update_status(".@%s No items found! Try another term!" % querier,
+        	in_reply_to_status_id = status.id)           
+    
     def on_error(self, status_code):
         return True
 
